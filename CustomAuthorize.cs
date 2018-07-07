e4d263a5a95d4fa999b064afd3fcd83087067b76
asaakumawat <asaakumawat@gmail.com>
2018-07-07 16:16:03 +0530

    Custom HTML Helper
    Custom HTML Helper For Authorized HTML Content,, Authorization from Database

---
+++ CustomAuthorize.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DS4UEMR.Domain.Concrete;
+using DS4UEMR.Domain.Entities;
+
+namespace DS4UEMR.Web.Common
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        public string ModuleName { get; set; }
+        public string ActionType { get; set; }
+        private string[] AllActions = new string[] { "write", "read", "delete" };
+        public EFPracticeRepository objPracticeRepo = new EFPracticeRepository();
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (HttpContext.Current != null)
+            {
+                if (HttpContext.Current.Session != null)
+                {
+                    //check that super admin login 
+                    if (HttpContext.Current.Session["AdminUserId"] != null)
+                    {
+                        return;
+                    }
+                    int practiceUserId = Convert.ToInt32(HttpContext.Current.Session["IDPracticeUser"]);
+                    int practiceId = Convert.ToInt32(HttpContext.Current.Session["PracticeId"]);
+                    bool isAuthorizedUser = IsUserAuthorized(practiceUserId,practiceId, ModuleName, ActionType);
+                    if(isAuthorizedUser==false)
+                    {
+                        filterContext.Result = new RedirectResult("/Account/AccessDenied");
+                    }
+                }
+                else
+                {
+                    filterContext.Result = new RedirectResult("/Account/Login");
+                }
+            }
+            else
+            {
+                filterContext.Result = new RedirectResult("/Account/Login");
+            }
+        }
+
+        public bool IsUserAuthorized(int practiceUserId,int practiceId,string ModuleName,string ActionType)
+        {
+            var userRolesList = objPracticeRepo.UserAuthorizetion(practiceUserId,practiceId, ModuleName);
+            if (userRolesList != null && !string.IsNullOrEmpty(ActionType))
+            {
+                UserAuthorizetion userRole;
+                ActionType = ActionType.ToLower();
+                if (AllActions.Contains(ActionType))
+                {
+                    userRole = userRolesList.Where(a =>
+                        ((ActionType == "write") && ((a.RoleIsWriteable == true && a.UserIsWriteable == null) || a.UserIsWriteable == true))
+                        ||
+                        ((ActionType == "read") && ((a.RoleIsReadable == true && a.UserIsReadable == null) || a.UserIsReadable == true))
+                        ||
+                        ((ActionType == "delete") && ((a.RoleIsDeleteable == true && a.UserIsDeleteable == null) || a.UserIsDeleteable == true))
+                        ).FirstOrDefault();
+                }
+                else
+                {
+                    userRole = userRolesList.Where(a =>
+                      (!string.IsNullOrEmpty(a.RoleAnonymous) ? a.RoleAnonymous : "").ToLower().Contains(ActionType)
+                       ).FirstOrDefault();
+                }
+                if (userRole == null)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool IsAuthorizedModule(string ModuleName,string ActionType)
+        {
+            int practiceUserId = Convert.ToInt32(HttpContext.Current.Session["IDPracticeUser"]);
+            int practiceId = Convert.ToInt32(HttpContext.Current.Session["PracticeId"]);
+            bool isAuthorizedModule = IsUserAuthorized(practiceUserId, practiceId, ModuleName, ActionType);
+            return isAuthorizedModule;
+        }
+    }
+
+    
+
+}
---
+++ CustomHtmlHelpers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Mvc.Html;
+using System.Text;
+
+namespace DS4UEMR.Web.Common
+{
+    public static class ActionLinkExtensions
+    {
+        public static MvcHtmlString ContentAuthorized(this HtmlHelper htmlHelper, AuthorizeParameters authParams, Func<object, object> htmlContent)
+        {
+            if (htmlHelper.ActionAuthorized(authParams))
+            {
+                string html = Convert.ToString(htmlContent.Invoke(null));
+                return new MvcHtmlString(html);
+            }
+            else
+            {
+                return MvcHtmlString.Empty;
+            }
+        }
+    }
+
+    public static class ActionExtensions
+    {
+        //This is used to get controller name its actions and  user authorization
+        internal static bool ActionAuthorized(this HtmlHelper htmlHelper, AuthorizeParameters authParams)
+        {
+            if (HttpContext.Current != null)
+            {
+                if (HttpContext.Current.Session != null)
+                {
+                    //check that super admin login 
+                    if (HttpContext.Current.Session["AdminUserId"] != null)
+                    {
+                        return true;
+                    }
+                    CustomAuthorizeAttribute customAuthrozation = new CustomAuthorizeAttribute();
+                    int practiceUserId = Convert.ToInt32(HttpContext.Current.Session["IDPracticeUser"]);
+                    int practiceId = Convert.ToInt32(HttpContext.Current.Session["PracticeId"]);
+                    bool IsAuthorizedUser = customAuthrozation.IsUserAuthorized(practiceUserId, practiceId, authParams.ModuleName, authParams.ActionType);
+                    return IsAuthorizedUser;
+                }
+            }
+            return false;
+        }
+    }
+    public class AuthorizeParameters
+    {
+        public string ModuleName { get; set; }
+        public string ActionType { get; set; }
+    }
+
+}
